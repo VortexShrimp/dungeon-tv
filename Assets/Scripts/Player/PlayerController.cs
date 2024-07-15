9979305dbbaa6031f125802e90046e12a4b3a836
ChrisFork <chrisventer.sa@gmail.com>
2024-07-15 23:45:15 +0200

    Gun aims at crosshair and flies around Player

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -11,6 +12,7 @@ namespace Player
         [SerializeField] Sprite[] _playerSprites;
         [SerializeField] GameObject _bulletPrefab;
         [SerializeField] SpriteRenderer _weaponSpriteRenderer;
+        [SerializeField] Transform _weaponTransform;
 
         Rigidbody2D _rigidbody;
         SpriteRenderer _spriteRenderer;
@@ -56,12 +58,26 @@ namespace Player
         void FixedUpdate()
         {
             HandleMovement();
+            GunPos();
         }
 
         //
         // Private class methods.
         //
 
+        // Make gun aim at crosshair & offset gun from player
+        void GunPos() {
+            var _mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            _mousePos.z = 0;
+            double _angle = Math.Atan2((_rigidbody.position.y - _mousePos.y),(_rigidbody.position.x - _mousePos.x)) * (180/Math.PI) - 180;
+            _weaponTransform.rotation = Quaternion.Euler(0,0,(float)_angle);
+            // Flips sprite accordingly
+            if (_angle < -270 || _angle > -90) _weaponSpriteRenderer.flipY = false;
+            else _weaponSpriteRenderer.flipY = true;
+            double _gunPosX = Math.Cos(_angle* Math.PI/180);
+            double _gunPosY = Math.Sin(_angle* Math.PI/180);
+            _weaponTransform.position = new Vector2((float)_gunPosX + _rigidbody.position.x,(float)_gunPosY + _rigidbody.position.y);
+        }
         // Move the player & adjust their sprite.
         void HandleMovement()
         {
@@ -85,13 +101,11 @@ namespace Player
                     // Moving right.
                     if (vel.x > 0)
                     {
-                        _weaponSpriteRenderer.flipX = false;
                         _spriteRenderer.flipX = true;
                     }
                     // Moving left.
                     else
                     {
-                        _weaponSpriteRenderer.flipX = true;
                         _spriteRenderer.flipX = false;
                     }
                 }
